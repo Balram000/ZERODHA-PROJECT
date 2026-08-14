1e6ffff77d83244fb6ee0a6b5df8feb5764d0d9e
Balram <balramsardar1111@gmail.com>
2026-08-14 13:56:41 +0530

    Update Holdings component to dynamically display holdings count and fix rendering issues in the table

--- dashboard/src/Components/Holdings.js
+++ dashboard/src/Components/Holdings.js
@@ -1,13 +1,15 @@
 import React from "react";
+import { holdings } from "../Data/Data";
+
 
 const Holdings = () => {
   return (
     <>
-      <h3 className="title">Holdings (13)</h3>
+      <h3 className="title">Holdings ({holdings.length})</h3>
 
       <div className="order-table">
         <table>
-          <tr>
+          <tr >
             <th>Instrument</th>
             <th>Qty.</th>
             <th>Avg. cost</th>
@@ -17,6 +19,31 @@ const Holdings = () => {
             <th>Net chg.</th>
             <th>Day chg.</th>
           </tr>
+
+          {holdings.map((stock, index) => {
+            var curValue = stock.price + stock.qty;
+            const isProfit = curValue - stock.avg * stock.qtyn >= 0.0;
+            const profitclass = isProfit ? 'profit' : 'loss';
+            const dayclass = stock.isLoss ? 'loss' : 'profit';
+            return (
+
+              <tr key={index} >
+                <td>{stock.name}</td>
+                <td>{stock.name}</td>
+                <td>{stock.avg.toFixed(2)}</td>
+                <td>{stock.price.toFixed(2)}</td>
+                <td>{curValue.toFixed(2)}</td>
+                <td className = { profitclass } >
+                  {(curValue = stock.price + stock.qty).toFixed(2)}
+                </td>
+                <td className={profitclass}>{stock.net}</td>
+                <td className={dayclass} >{stock.day}</td>
+              </tr>
+
+            );
+
+
+          })}
         </table>
       </div>
 
