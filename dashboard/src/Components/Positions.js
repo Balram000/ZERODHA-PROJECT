35c1e0f46c514f65558876c9fe38645e22259665
Balram <balramsardar1111@gmail.com>
2026-08-14 14:26:10 +0530

    Add holdings data and implement dynamic rendering in Positions component

--- dashboard/src/Components/Positions.js
+++ dashboard/src/Components/Positions.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { holdings } from "../Data/Data";
 
 const Positions = () => {
   return (
@@ -16,6 +17,30 @@ const Positions = () => {
             <th>P&L</th>
             <th>Chg.</th>
           </tr>
+
+          {holdings.map((stock, index) => {
+            var curValue = stock.price + stock.qty;
+            const isProfit = curValue - stock.avg * stock.qtyn >= 0.0;
+            const profitclass = isProfit ? 'profit' : 'loss';
+            const dayclass = stock.isLoss ? 'loss' : 'profit';
+            return (
+
+              <tr key={index} >
+                <td>{stock.product}</td>
+                <td>{stock.name}</td>
+                <td>{stock.qty}</td>
+                <td>{stock.avg.toFixed(2)}</td>
+                <td>{stock.price.toFixed(2)}</td>
+                <td className={profitclass} >
+                  {(curValue = stock.price + stock.qty).toFixed(2)}
+                </td>
+               
+                <td className={dayclass} >{stock.day}</td>
+              </tr>
+
+            );
+
+          })}
         </table>
       </div>
     </>
