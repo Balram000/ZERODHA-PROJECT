9cd6cbf2c6a9383f0c4b95e2d3e2a3429571f5a4
Balram <balramsardar1111@gmail.com>
2026-08-16 22:53:06 +0530

    refactor: simplify routing structure by removing unused components and switching to Routes

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,76 +1,16 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider,Outlet,} from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
-import Home from './Components/Home'
-import Holdings from "./Components/Holdings";
-import Menu from "./Components/Menu";
-import Dashboard from "./Components/Dashboard";
-import Positions from "./Components/Positions";
-import Orders from "./Components/Orders";
-import TopBar from "./Components/TopBar";
-import WatchList from "./Components/WatchList";
-import Funds from "./Components/Funds";
-import Apps from "./Components/Apps";
+import Home from "./Components/Home";
 
-
-function Layout() {
-  return (
-    <div>
-      <TopBar />
-      <Outlet />
-  
-    </div>
-  );
-}
-
-
-
-const router = createBrowserRouter([
-  {
-    
-    element:  <Layout/> ,
-  children :[
-  {
-    path: "/",
-    element: <> <Home/> </>,
-  },
-  {
-    path: "/Holding",
-    element: <> <Holdings/> </>,
-  },
-  {
-    path: "/Menu",
-    element: <> <Menu/> </>,
-  },
-  {
-    path: "/Dashboard",
-    element: <> <Dashboard/> </>,
-  },
-  {
-    path: "/Positions",
-    element: <> <Positions/> </>,
-  },
-  {
-    path: "/Orders",
-    element: <> <Orders/> </>,
-  },
-  {
-    path: "/funds",
-    element: <> <Funds/> </>,
-  },
-  {
-    path: "/Apps",
-    element: <> <Apps/> </>,
-  },
-  {
-    path: "/WatchList",
-    element: <> <WatchList/> </>,
-  },
- ],
-},
-]);
-
-createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
