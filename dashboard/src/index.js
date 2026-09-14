5d155b8c23da690ae7089956c35d7b72d8ae985a
Balram <balramsardar1111@gmail.com>
2026-09-14 11:50:15 +0530

    feat: update package.json homepage and configure nginx for dashboard routing

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -22,7 +22,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/dashboard">
       <Routes>
         <Route path="/apps" element={<AppsPage />} />
         <Route path="/*" element={<Home />} />
